c4c27a04078f443b95675f5dbbeb2c1af36c111c
misha trunev <nove48@yandex.ru>
2021-11-06 14:28:59 +0300

    Сделал первоначальную настройку и добавил Identity Server.

---
+++ Events/Models/DBContext.cs
@@ -0,0 +1,23 @@
+﻿using Events.Models.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events.Models
+{
+    public class DBContext : IdentityDbContext<ApplicationUser>
+    {
+        public DBContext(DbContextOptions<DBContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(typeof(Startup).Assembly);
+            base.OnModelCreating(builder);
+        }
+    }
+}
--- Events/Startup.cs
+++ Events/Startup.cs
@@ -1,7 +1,11 @@
+using Events.Models;
+using Events.Models.Entities;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -28,6 +32,10 @@ namespace Events
         {
 
             services.AddControllers();
+            services.AddDbContext<DBContext>(options=>
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddIdentity <ApplicationUser, IdentityRole>()
+               .AddEntityFrameworkStores<DBContext>();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Events", Version = "v1" });
@@ -44,10 +52,10 @@ namespace Events
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Events v1"));
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
